8e1b319ba7632dcf0bdfbfde35dac1cb793957c9
Daniel Andreas Wang <danielaw@getacademy.no>
2025-11-25 11:22:09 +0100

    refine with handleClick

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,13 +7,19 @@ const props = defineProps<{
 const emit = defineEmits<{
   (e: 'opened'): void;
 }>();
+
+const handleClick = () => {
+  if (!props.isOpen) {
+    emit('opened')
+  }
+};
 </script>
 
 <template>
   <div
     class="card"
     :class="{ isOpen: props.isOpen }"
-    @click="!props.isOpen && emit('opened')"
+    @click="handleClick"
   >
     <span class="emoji" v-if="props.isOpen">{{ props.emoji }}</span>
     <span v-else>?</span>
