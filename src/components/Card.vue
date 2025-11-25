d2fe1f7bae10677b145bf986cff3ec6d2f53765b
Daniel Andreas Wang <danielaw@getacademy.no>
2025-11-25 11:20:05 +0100

    add card with props and emits

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,2 +1,21 @@
 <script setup lang="ts">
+const props = defineProps<{
+  emoji: string;
+  isOpen: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: 'opened'): void;
+}>();
 </script>
+
+<template>
+  <div
+    class="card"
+    :class="{ isOpen: props.isOpen }"
+    @click="!props.isOpen && emit('opened')"
+  >
+    <span class="emoji" v-if="props.isOpen">{{ props.emoji }}</span>
+    <span v-else>?</span>
+  </div>
+</template>
